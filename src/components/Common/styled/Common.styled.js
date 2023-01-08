bfa7a5ba58eb1ff49fbdc8ba512d969ad2a7d752
Kseniia30 <kellyshikova3004@gmail.com>
2023-01-08 21:43:22 +0200

    add changes to font and color to text

--- src/components/Common/styled/Common.styled.js
+++ src/components/Common/styled/Common.styled.js
@@ -8,7 +8,10 @@ export const Container = styled.div`
     padding-right: 20px;
     padding-top: 10px;
     padding-bottom: 10px;
-    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2));
+    background-image: linear-gradient(
+        rgba(255, 255, 255, 0.2),
+        rgba(255, 255, 255, 0.2)
+    );
 
     @media screen and (min-width: 768px) {
         width: 768px;
@@ -18,7 +21,7 @@ export const Container = styled.div`
 
     @media screen and (min-width: 1200px) {
         width: 1200px;
-        padding-left: 10px;
-        padding-right: 10px;
+        padding-left: 30px;
+        padding-right: 30px;
     }
 `;
